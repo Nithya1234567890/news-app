6906e1a30d56471f0b925a447a137ead6f9ee62f
Nithya1234567890 <nithyamunagapati@gmai.com>
2024-01-18 23:40:14 +0530

    changes

--- src/components/Newsnavbar.js
+++ src/components/Newsnavbar.js
@@ -13,7 +13,7 @@ export class Newsnavbar extends Component {
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+          <Link className="nav-link active" aria-current="page" to="/news-api">Home</Link>
         </li>
           <li className="nav-item"><Link className="nav-link" to="/business">business</Link></li>
           <li className="nav-item"><Link className="nav-link" to="/entertainment">entertainment</Link></li>
