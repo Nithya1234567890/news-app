4877a4e58f313881c426f91392a76c67cfb01818
Nithya1234567890 <nithyamunagapati@gmai.com>
2024-01-28 17:03:34 +0530

    changing..

--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -6,14 +6,14 @@ const Newsitem =(props)=>{
       <div>
         <div className="container my-4" id="item">
           <div className="card">
-          <span class="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left:'92%',zIndex:'1'}}>{source}</span>
+          <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left:'92%',zIndex:'1'}}>{source}</span>
             <img src={imgurl} className="card-img-top" alt="..." />
             <div className="card-body">
               <h5 className="card-title">{title}</h5>
               <p className="card-text">
                 {description}
               </p>
-              <p class="card-text"><small class="text-muted">By {author} on {new Date(date).toDateString()}</small></p>
+              <p className="card-text"><small className="text-muted">By {author} on {new Date(date).toDateString()}</small></p>
               <a target="blank" href={url} className="btn btn-dark">
                 Read More
               </a>
